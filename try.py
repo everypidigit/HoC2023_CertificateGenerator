5afb0d4f560ac4da463f8b070cc7365756702a5b
everypidigit <everypidigit@gmail.com>
2023-12-02 15:21:36 +0600

    initial commit

--- try.py
+++ try.py
@@ -8,17 +8,18 @@ def generate_certificate(input_image_path, output_image_path, text_to_add):
     draw = ImageDraw.Draw(image)
 
     # Choose a font and size
-    font = ImageFont.load_default()  # Use the default PIL font
-    font = font.font_variant(size=font_size)  # Set the font size
+    myFont = ImageFont.load_default()
+    myFont = ImageFont(65)
+    draw.text((1700, 1480), text_to_add, font=myFont, fill=(255, 0, 0))
 
-    # Choose text color
-    text_color = (255, 255, 255)  # RGB color tuple (white in this case)
+    # # Choose text color
+    # text_color = (255, 255, 255)  # RGB color tuple (white in this case)
 
-    # Choose text position
-    text_position = (1700, 1480)  # (x, y) coordinates
+    # # Choose text position
+    # text_position = (1700, 1480)  # (x, y) coordinates
 
-    # Add text to the image
-    draw.text(text_position, text_to_add, font=font, fill=text_color)
+    # # Add text to the image
+    # draw.text(text_position, text_to_add, font=font, fill=text_color)
 
     # Save the modified image
     image.save(output_image_path)
@@ -26,7 +27,7 @@ def generate_certificate(input_image_path, output_image_path, text_to_add):
 if __name__ == "__main__":
     input_image_path = "./certificatesRu/volunteerRu.jpg"  # Replace with your input image path
     output_image_path = "output_certificate.jpg"  # Replace with your desired output image path
-    text_to_add = "John Doe"  # Replace with the desired text
+    text_to_add = "APCHIIIASJDOIASHDOIAHS AOSIHDASIODH"  # Replace with the desired text
     font_size = 36
 
     generate_certificate(input_image_path, output_image_path, text_to_add)
