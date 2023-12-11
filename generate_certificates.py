86eb50b2a6112b681923a96450f3e59a70449615
everypidigit <everypidigit@gmail.com>
2023-12-11 18:51:27 +0600

    cleaned some code

--- generate_certificates.py
+++ generate_certificates.py
@@ -6,9 +6,9 @@ from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from email.mime.image import MIMEImage
 
-def send_email(subject, body, to_email, attachment_path):
-    sender_email = "everypidigit@gmail.com"  
-    sender_password = "lili qgkh zlnr apsy"
+def send_email(subject, body, to_email, cert_path, vouch_path):
+    sender_email = "daniyar@ustemrobotics.kz"  
+    sender_password = "afos vsor ermk crua"
     smtp_server = 'smtp.gmail.com'  
 
     message = MIMEMultipart()
@@ -16,52 +16,85 @@ def send_email(subject, body, to_email, attachment_path):
     message['To'] = to_email
     message['Subject'] = subject
 
-    with open(attachment_path, 'rb') as attachment:
-        image = MIMEImage(attachment.read(), _subtype='jpeg', name='HourOfCode2023_certificate.jpg')
-    message.attach(image)
+    with open(cert_path, 'rb') as attachment:
+        certificate = MIMEImage(attachment.read(), _subtype='jpeg', name='HourOfCode2023_certificate.jpg')
+    with open(vouch_path, 'rb') as attachment:
+        voucher = MIMEImage(attachment.read(), _subtype='jpeg', name='Voucher.jpg')
+    message.attach(certificate)
+    message.attach(voucher)
 
     message.attach(MIMEText(body, 'plain'))
 
     with smtplib.SMTP_SSL(smtp_server, 465) as server:
         server.login(sender_email, sender_password)
         server.sendmail(sender_email, to_email, message.as_string())
+        print(f"email sent to {to_email}")
         server.quit()
 
-def generate_certificate(input_image_path, output_image_path, text_to_add, email_address):
-    initImage = Image.open(input_image_path)
-    draw = ImageDraw.Draw(initImage)
-    myFont = ImageFont.truetype('./font/FreeMono.ttf', 120)
+def generate_certificate(input_image_path, voucher_path, output_image_path, output_voucher_path, text_to_add, email_address):
+    initCertificateImage = Image.open(input_image_path)
+    drawCertificate = ImageDraw.Draw(initCertificateImage)
+    
+    initVoucherImage = Image.open(voucher_path)
+    drawVoucher = ImageDraw.Draw(initVoucherImage)
+    
+    certificateFont = ImageFont.truetype('./font/FreeMono.ttf', 120)
+    voucherFont = ImageFont.truetype('./font/FreeMono.ttf', 80)
     
     # the location for printing is chosen as absolute pixels, so gotta make some wraparound so that
     # everything's printed beautifully
     # location for printing text is chosen based on the length of the name:
     # shorter names will be printed closer to the absolute center, etc
     if len(text_to_add) < 7:
-        draw.text((2200, 1390), text_to_add, font=myFont, fill=(255, 0, 0))
+        drawCertificate.text((2200, 1390), text_to_add, font=certificateFont, fill=(255, 0, 0))
+        drawVoucher.text((430, 1320), text_to_add, font=voucherFont, fill=(0, 0, 0))
         
     elif len(text_to_add) > 6 and len(text_to_add) < 10:
-        draw.text((2150, 1390), text_to_add, font=myFont, fill=(255, 0, 0))
+        drawCertificate.text((2150, 1390), text_to_add, font=certificateFont, fill=(255, 0, 0))
+        drawVoucher.text((3800, 1320), text_to_add, font=voucherFont, fill=(0, 0, 0))
         
     elif len(text_to_add) > 9 and len(text_to_add) < 13: 
-        draw.text((2050, 1390), text_to_add, font=myFont, fill=(255, 0, 0))
+        drawCertificate.text((2050, 1390), text_to_add, font=certificateFont, fill=(255, 0, 0))
+        drawVoucher.text((300, 1320), text_to_add, font=voucherFont, fill=(0, 0, 0))
         
     elif len(text_to_add) > 12:
-        draw.text((1850, 1390), text_to_add, font=myFont, fill=(255, 0, 0))
+        drawCertificate.text((1850, 1390), text_to_add, font=certificateFont, fill=(255, 0, 0))
+        drawVoucher.text((220, 1700), text_to_add, font=voucherFont, fill=(0, 0, 0))
 
     # saving the generated image
-    initImage.save(output_image_path)
+    # initCertificateImage.save(output_image_path)
+    initVoucherImage.save(output_voucher_path)
     
     
     # NOT SENDING ANYTHING TO EMAILS AS OF NOW
     # GOTTA FINALIZE THE EMAIL BODY TEXT
     # sending the generated image to the correct email address
-    # send_email(email_subject, email_body, email_address, output_image_path)
+    # send_email(email_subject, email_body, email_address, output_image_path, output_voucher_path)
         
 if __name__ == "__main__":
-    DF = pd.read_csv("/Users/daniyarkakimbekov/Workspaces/HoC2023_DataAnalytics/cleaned_data.csv")
-    DF = DF[0:30]
+    DF = pd.read_csv("./dec5.csv")
+    DF = DF[0:15]
     
-    email_body = "Құрметті дос, Дорогой участник, благодарим тебя за прохождение Часа Кода!/n Твой сертификат прикреплён к данному письму!"
+    email_body = """
+    
+    Дорогой друг тебя приветствует команда Час Кода Казахстан.
+
+    С 4 по 10 декабря мы проводим самую масштабную акцию по программированию для учащихся 5 - 11 классов в Казахстане.
+    Благодарим тебя за участие и успешное прохождение заданий.
+    В качестве подтверждения отправляем официальный именной сертификат участника, а также в знак нашей признательности вручаем тебе лимитированный ваучер на обучение в школе английского Step.
+
+    Организаторы акции:
+    ✅ USTEM Foundation. https://firstrobotics.kz/first-kaz
+    ✅StudySC
+
+    Партнеры:
+    ✅Республиканский научно-практический центр «Дарын» Министерства просвещения Республики Казахстан https://daryn.kz/о-центре/
+    ✅Step Europe s.r.o. https://steptoenglish.org/
+
+    Хочешь стать частью нашей команды?
+    Пиши в директ нашей официальной страницы в Instagram - @hourofcode.kz
+    
+    """
     email_subject = "Код Сағаты 2023 / Час Кода 2023. Сертификат. "
     
     for i in range(len(DF)):
@@ -98,11 +131,13 @@ if __name__ == "__main__":
             # here, we have a rigid folder structure. we have only Kazakh and Russian folders
             # we take the language we got from the data, append it to ./, then append a slach, then append the role we got from the data, then append the .jpg
             # final path shoud look something like this: ./kazakh/student.jpg
-            certificate_path = "".join(["./",language, "/",role, ".jpg"])
+            certificate_path = "".join(["./", language, "/", role, ".jpg"])
+            voucher_path = "".join(["./", language, "/voucher.jpg"])
             
             # building the correct output path and final filename:
             # final path should look something like this: ./certificates/daniyarkakimbekov.jpeg
             output_path = "".join(["./certificates/",role, "/", name_for_path, ".jpeg"])
+            out_voucher_path = "".join(["./certificates/",role, "/", name_for_path, "_Voucher.jpeg"])
             
             # we pass correct certficate path and the correct output path to the function
             # on top of that, we pass the name that we got from the data
@@ -111,6 +146,6 @@ if __name__ == "__main__":
             # the actual email sending is not done now for testing purposes
             # we can also implement sending a Kazakh/Russian email based on the registration data.
             # gotta pass the language data in here, then save it as some variable inside generate_certificate, then pass this variable to the send_email
-            # generate_certificate(certificate_path, output_path, name, "everypidigit@gmail.com")
+            generate_certificate(certificate_path, voucher_path, output_path, out_voucher_path, name, "everypidigit@gmail.com")
 
     print("FINISHED THE WHOLE PROCESS")
