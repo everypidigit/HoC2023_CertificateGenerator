e8ea8e38cd11bba7ffc51426d2fd6994d88e381a
everypidigit <everypidigit@gmail.com>
2023-12-28 12:41:42 +0600

    many changes, added email checking

---
+++ .ipynb_checkpoints/gen-checkpoint.py
@@ -0,0 +1,75 @@
+from PIL import Image, ImageDraw, ImageFont
+import pandas as pd
+import re
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.image import MIMEImage
+import csv
+from datetime import datetime
+
+def generate_certificate(input_image_path, output_image_path, name):
+    try:
+        initCertificateImage = Image.open(input_image_path)
+        drawCertificate = ImageDraw.Draw(initCertificateImage)
+        
+    except Exception:
+        print("huh")
+        pass
+    
+    certificateFont = ImageFont.truetype('./font/FreeMono.ttf', 80)
+    
+    if len(name) < 7:
+        drawCertificate.text((2200, 1420), name, font=certificateFont, fill=(255, 0, 0))
+        
+    elif len(name) > 6 and len(name) < 10:
+        drawCertificate.text((2150, 1420), name, font=certificateFont, fill=(255, 0, 0))
+ 
+    elif len(name) > 9 and len(name) < 13: 
+        drawCertificate.text((2050, 1420), name, font=certificateFont, fill=(255, 0, 0))
+        
+    elif len(name) > 12 and len(name) < 20:
+        drawCertificate.text((1870, 1420), name, font=certificateFont, fill=(255, 0, 0))
+        
+    elif len(name) >= 20 and len(name) < 26:
+        drawCertificate.text((1800, 1420), name, font=certificateFont, fill=(255, 0, 0))
+        
+        
+    elif len(name) >= 26 and len(name) < 30:
+        drawCertificate.text((1740, 1420), name, font=certificateFont, fill=(255, 0, 0))
+
+    elif len(name) >= 30:
+        drawCertificate.text((1590, 1420), name, font=certificateFont, fill=(255, 0, 0))
+
+    original_width, original_height = initCertificateImage.size
+    new_width = int(original_width * 0.3)
+    new_height = int(original_height * 0.3)    
+    initCertificateImage = initCertificateImage.resize((new_width, new_height))
+    
+    try:
+        initCertificateImage.save(output_image_path)
+    except Exception:
+        print("huh2")
+        pass
+        
+if __name__ == "__main__":
+    DF = pd.read_excel("/Users/daniyarkakimbekov/Workspaces/HoC2023_CertificateGenerator/specialData/Margulan27dec.csv")
+    
+    for i in range(0,len(DF)):
+            
+            name = str(DF["name"][i]).lower().split()
+            capitalized_strings = [s.capitalize() for s in name]
+            name = ' '.join(capitalized_strings)
+        
+            name_for_path_dirty = str(DF["name"][i])
+            patterns_to_remove = ['https://www.', '/', ',', '&', '^', '%', '$', '#', '.']
+            pattern = '|'.join(re.escape(p) for p in patterns_to_remove)
+            name_for_path = re.sub(pattern, '', name_for_path_dirty)
+            
+            certificate_path = "./kazakh/volunteer.jpg"
+            
+            output_path = "".join(["./certificates/volunteer/", name_for_path, ".jpeg"])
+            
+            print(f"starting process for user number {i} name {name}")
+            generate_certificate(certificate_path, output_path, name)
+
--- dev.py
+++ dev.py
@@ -7,11 +7,17 @@ from email.mime.text import MIMEText
 from email.mime.image import MIMEImage
 import csv
 from datetime import datetime
+import sys
+from email_validator import validate_email, EmailNotValidError
+
+def validation_check(email):
+    try:
+        email = validate_email(email)
+        return True
+    except EmailNotValidError as e:
+        print(f"for email {email} the error is: {str(e)}")
 
 def send_email(subject, body, to_email, cert_path, vouch_path, address, student_name):
-    
-    print("entering sending email")
-    
     message = MIMEMultipart()
     message['From'] = address
     message['To'] = to_email
@@ -29,7 +35,7 @@ def send_email(subject, body, to_email, cert_path, vouch_path, address, student_
     try:
         server.sendmail(address, to_email, message.as_string())
         
-        print("sent email")
+        print("SENT EMAIL")
         
         timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
         row = [student_name, to_email, timestamp]
@@ -38,22 +44,28 @@ def send_email(subject, body, to_email, cert_path, vouch_path, address, student_
             csv_writer = csv.writer(csv_file)
             csv_writer.writerow(row)
         
-    except Exception:
+    except Exception as e:
         timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
         row = [student_name, to_email, timestamp]
+        
+        print("                      ")
+        print("THE EMAIL WAS NOT SENT")
+        print("                      ")
+        print({e})
         
         with open(bad_log_path, 'a', newline='') as csv_file:
             csv_writer = csv.writer(csv_file)
             csv_writer.writerow(row)
-        
-        pass
+            
+        if "502" in e:
+            pass
+        else:
+            sys.exit("THE PROGRAM EXECUTION WAS CUT SHORT BECAUSE THE EMAIL HAS NOT BEEN SENT")
     
 def generate_certificate(input_image_path, voucher_path, output_image_path, output_voucher_path, text_to_add, email_address, addr):
-    print("entering generate certificate")
     try:
         initCertificateImage = Image.open(input_image_path)
         drawCertificate = ImageDraw.Draw(initCertificateImage)
-        print("generated cert")
         
     except Exception:
         timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
@@ -67,7 +79,6 @@ def generate_certificate(input_image_path, voucher_path, output_image_path, outp
     
     initVoucherImage = Image.open(voucher_path)
     drawVoucher = ImageDraw.Draw(initVoucherImage)
-    print("generated vouch")
     
     certificateFont = ImageFont.truetype('./font/FreeMono.ttf', 120)
     voucherFont = ImageFont.truetype('./font/FreeMono.ttf', 80)
@@ -115,9 +126,11 @@ def generate_certificate(input_image_path, voucher_path, output_image_path, outp
     send_email(email_subject, email_body, email_address, output_image_path, output_voucher_path, addr, text_to_add)
         
 if __name__ == "__main__":
-    DF = pd.read_csv("./final_full.csv")
+    DF = pd.read_csv("./data/clean_final.csv")
+    bad_log_path = "./bad_email.csv"
+    good_log_path = "./good_email.csv"
     
-    limit = 7290+500+500+500
+    limit = 19876+500+500+500
     
     DF = DF[limit:]
 
@@ -125,16 +138,19 @@ if __name__ == "__main__":
     user = "hoc2023certificates1@hourofcode.kz"
     password = "Rapture9949!"
     
+    smtp_study = "mail.studycs.kz"
+    user_study = "hoc2023certificates1@studycs.kz"
+    password_study = "Rapture9949!"
     
-    smtp_ustem = 'smtp.gmail.com'
+    smtp_google = 'smtp.gmail.com'
     user_ustem = "daniyar@ustemrobotics.kz"  
     password_ustem = "afos vsor ermk crua"
     
+    # user = "everypidigit@gmail.com"
+    # password = "yziz tldl uxot ahwj"
+
     # user = "astana.code@gmail.com"
     # password = "ysrv aetx fiim qddw"
-    
-    bad_log_path = "./bad_email.csv"
-    good_log_path = "./good_email.csv"
     
     email_body = """
     
@@ -146,57 +162,64 @@ if __name__ == "__main__":
 
 Организаторы акции:
 ✅ USTEM Foundation. https://firstrobotics.kz/first-kaz
-✅StudySC
+✅ StudySC
 
 Партнеры:
-✅Республиканский научно-практический центр «Дарын» Министерства просвещения Республики Казахстан https://daryn.kz/о-центре/
-✅Step Europe s.r.o. https://steptoenglish.org/
+✅ Республиканский научно-практический центр «Дарын» Министерства просвещения Республики Казахстан https://daryn.kz/о-центре/
+✅ Step Europe s.r.o. https://steptoenglish.org/
 
 Хочешь стать частью нашей команды?
 Пиши в директ нашей официальной страницы в Instagram - @hourofcode.kz
-    
+
+Бұл электрондық пошта автоматты түрде жасалды. Жауап бермеңіз.
+Это письмо было сгенерировано автоматически. Пожалуйста, не отвечайте на него.
     """
     email_subject = "Код Сағаты 2023 / Час Кода 2023. Сертификат"
     
-    with smtplib.SMTP_SSL(smtp_server, 465) as server:
-        
-            server.login(user, password)
-            
-            for i in range(0,500):
+    with smtplib.SMTP_SSL(smtp_study, 465) as server:
+            server.login(user_study, password_study)
+            for i in range(206,500):
                 
                 index = limit+i
-                role = str(DF["role"][index]).replace(" ", "")
-            
-                if role == "teacher" or role == " teacher" or role == "volunteer":
-                    pass
-                
-                language = str(DF["language"][index]).replace(" ", "")
-                
-                
-                
-                if language == "english":
-                    language = "kazakh"
                 
                 name = str(DF["name"][index]).lower().split()
                 capitalized_strings = [s.capitalize() for s in name]
                 name = ' '.join(capitalized_strings)
-            
-                name_for_path_dirty = str(DF["name"][index])
-                patterns_to_remove = ['https://www.', '/', ',', '&', '^', '%', '$', '#', '.']
-                pattern = '|'.join(re.escape(p) for p in patterns_to_remove)
-                name_for_path = re.sub(pattern, '', name_for_path_dirty)
                 
                 participant_email = str(DF["email"][index]).replace(" ", "")
                 
-                certificate_path = "".join(["./", language, "/", role, ".jpg"])
-                voucher_path = "".join(["./", language, "/voucher.jpg"])
+                if validation_check(participant_email):
                 
-                output_path = "".join(["./certificates/",role, "/", name_for_path, ".jpeg"])
-                out_voucher_path = "".join(["./certificates/",role, "/", name_for_path, "_Voucher.jpeg"])
+                    role = str(DF["role"][index]).replace(" ", "")
+                    language = str(DF["language"][index]).replace(" ", "")
                 
-                print(f"starting process for user number {i}, email {participant_email}, name {name}")
-                generate_certificate(certificate_path, voucher_path, output_path, out_voucher_path, name, participant_email, user)
+                    if role == "teacher" or role == " teacher" or role == "volunteer" or role == " volunteer":
+                        pass
+                    
+                    if language == "english":
+                        language = "kazakh"
+            
+                    name_for_path_dirty = str(DF["name"][index])
+                    patterns_to_remove = ['https://www.', '/', ',', '&', '^', '%', '$', '#', '.']
+                    pattern = '|'.join(re.escape(p) for p in patterns_to_remove)
+                    name_for_path = re.sub(pattern, '', name_for_path_dirty)
+                    
+                    certificate_path = "".join(["./templates/", language, "/", role, ".jpg"])
+                    voucher_path = "".join(["./templates/", language, "/voucher.jpg"])
+                    
+                    output_path = "".join(["./certificates/",role, "/", name_for_path, ".jpeg"])
+                    out_voucher_path = "".join(["./certificates/",role, "/", name_for_path, "_Voucher.jpeg"])
+                    
+                    print(f"starting process for user number {i}, email {participant_email}, name {name}")
+                    generate_certificate(certificate_path, voucher_path, output_path, out_voucher_path, name, participant_email, user_ustem)
+                    
+                else: 
+                    timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+                    row = [name, participant_email, timestamp]
+                    
+                    with open(bad_log_path, 'a', newline='') as csv_file:
+                        csv_writer = csv.writer(csv_file)
+                        csv_writer.writerow(row)
     
 
     print("FINISHED SENDING 500 EMAILS")
-    # server.quit()
